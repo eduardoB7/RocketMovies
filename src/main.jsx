ab01c653a45ca9aa8190c86a1a3e87496ffd7b4b
eduardob7 <edu77barroso@hotmail.com>
2023-12-13 10:42:33 -0300

    inicio da estrutura do header e pagina home

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import theme from "./styles/theme";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+    </ThemeProvider>
+  </React.StrictMode>
+);
---
+++ src/styles/global.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+:root{
+    font-size: 62.5%;
+}
+body{
+    font-size: 1.6rem;
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_DEFAULT};
+}
+`;
