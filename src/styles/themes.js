532fad440566b40777790972faa100062c08bd28
eduardob7 <edu77barroso@hotmail.com>
2023-12-13 09:55:21 -0300

    arquivo com temas comuns no app

---
+++ src/styles/themes.js
@@ -0,0 +1,11 @@
+export default {
+  COLORS: {
+    BACKGROUND_DEFAULT: "#1C1B1E",
+    TITLE_BUTTON: "#FF859B",
+    PLACEHOLDER: "#948F99",
+
+    WHITE: "#F4EDE8",
+    GRAY: "#CAC4CF",
+    DARK: "#312E38",
+  },
+};
