2934226d346f714c461fb30861319b3f4c26c132
Eduardo Barroso <eduardoB7.dev@hotmail.com>
2023-12-17 17:06:07 -0300

    scroll bar

--- src/styles/global.js
+++ src/styles/global.js
@@ -16,4 +16,23 @@ body{
     font-family: 'Roboto Slab', serif;
     height: 100vh;
 }
+
+  /* Estilizando a barra de rolagem */
+  /* Estilizando a barra */
+  ::-webkit-scrollbar {
+    width: 8px; /* Largura da barra de rolagem */
+}
+
+/* Estilizando o track (trilha) */
+::-webkit-scrollbar-track {
+    background: transparent; /* Cor da trilha */
+}
+
+/* Estilizando o thumb (a "bolinha" da barra de rolagem) */
+::-webkit-scrollbar-thumb {
+      border-radius: 8px;
+    background: #FF859B; /* Cor da bolinha */
+  }
+
+
 `;
