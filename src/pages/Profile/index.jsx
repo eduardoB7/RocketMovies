6e98c37f52002bf0461abf19816ed301fd933ba6
eduardob7 <edu77barroso@hotmail.com>
2024-01-05 12:09:33 -0300

    profile update

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,6 +1,8 @@
-import { Container } from "./style";
+import { Container, FormProfile, Avatar } from "./style";
 
 import { GoArrowLeft } from "react-icons/go";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+
 import { ButtonText } from "../../components/ButtonText";
 
 export function Profile() {
@@ -9,6 +11,16 @@ export function Profile() {
       <header>
         <ButtonText icon={GoArrowLeft} texContent={"Voltar"} />
       </header>
+      <FormProfile>
+        <Avatar>
+          <img src="https://www.github.com/eduardoB7.png" alt="" />
+          <label htmlFor="avatar">
+            <FiCamera />
+
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+      </FormProfile>
     </Container>
   );
 }
